0c6eab80d623475983358676ae3f398ab7562cfa
xluoyu <xluoyu@users.noreply.github.com>
2023-01-05 01:05:30 +0000

    deploy: a4e361000dfbf902c556426656d0f3b050a21ae1

--- assets/chunks/copy.74f5011b.js
+++
@@ -1,1 +0,0 @@
-import{q as t}from"../app.75fc0271.js";const n=o=>{const e=document.createElement("input");e.value=o,document.body.appendChild(e),e.select(),document.execCommand("Copy"),document.body.removeChild(e),t({message:"复制成功",type:"primary"})};export{n as o};
